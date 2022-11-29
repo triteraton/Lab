d34ec2e4f1d4bc7097c8ad16257fe386c18d1676
triteraton <71770032+triteraton@users.noreply.github.com>
2022-11-29 21:19:14 +0300

    Add files via upload

---
+++ C#/Lab_1/Lab_1/A.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A
+{
+    class A
+    {
+        private decimal a = 15m;
+        private decimal b = 4m;
+
+        public float c
+        {
+            get { 
+                return (float)(a++ - b); 
+            }
+        }
+    }
+}
---
+++ C#/Lab_1/Lab_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            A aObject = new A();
+            Console.WriteLine($"Result: {aObject.c}");
+        }
+    }
+}
