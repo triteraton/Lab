48fe59f7aa5069f2ea2163faa0093c6d62a658b2
triteraton <71770032+triteraton@users.noreply.github.com>
2022-11-29 21:25:37 +0300

    Add files via upload

---
+++ C#/Lab_3/B.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    class B : A
+    {
+        private decimal d = 0m;
+        private decimal[] arr = Array.Empty<decimal>();
+        public B(decimal D, decimal A, decimal B) : base(A, B)
+        {
+            this.d = D;
+        }
+
+        public B(decimal D, decimal A, decimal B, int coef) : this(D, A, B)
+        {
+            arr = new decimal[(int)A + coef];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = c2 * i;
+            }
+        }
+
+        public void printArr()
+        {
+            int k = 0;
+            foreach (int i in arr)
+            {
+                Console.WriteLine($"arr [{k++}]: {i}");
+
+            }
+
+        }
+        public decimal c2
+        {
+            get
+            {
+                do
+                {
+                    d -= d - c;
+                } while (d >= 5);
+                return d;
+            }
+        }
+    }
+}
---
+++ C#/Lab_4/Lab_4/C.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    class C<Param>
+    {
+        public static String value = "static";
+        private Param val;
+
+        public Param forParam
+        {
+            get { return val; }
+            set { val = value; }
+        }
+    }
+}
